e6c04c56105b485469aaa5e6039dbf45b7bcb83b
jeffxu71 <jeffxu71@gmail.com>
2018-05-19 16:31:42 -0400

    P02

--- P02.scala
+++ P02.scala
@@ -1,9 +1,13 @@
 
 object P02 extends App {
-  def penultimate[T](l: List[T]): T = {
-    l.reverse.tail.head
+  def penultimate[T](l: List[T]): T = l match {
+    case h :: _ :: Nil => h
+    case _ :: tail => penultimate(tail)
+    case _ => throw new NoSuchElementException
   }
 
   println(penultimate(List(1,1,2,3,5,8)))
   println(penultimate(List('a','b','c','d','e','f')))
+  // Case below will cause an exception thrown
+  //println(penultimate(List('a')))
 }
