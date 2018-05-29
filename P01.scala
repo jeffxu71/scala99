c1c1ff2f955e9ea16602b25a3469ca371099f34d
jeffxu71 <jeffxu71@gmail.com>
2018-05-28 22:37:35 -0400

    update P01

--- P01.scala
+++ P01.scala
@@ -7,13 +7,6 @@ object P01 extends App {
     case _ => throw new NoSuchElementException
   }
 
-  def last2[T](l: List[T]): T = {
-    l.reverse.head
-  }
-
   println(last(List(1,1,2,3,5,8)))
   println(last(List('a','b','c','d','e')))
-
-  println(last2(List(1,1,2,3,5,8)))
-  println(last2(List('a','b','c','d','e')))
 }
