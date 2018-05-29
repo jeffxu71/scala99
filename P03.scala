959f2deb0c6189e5900b53be9248a052540f3be6
jeffxu71 <jeffxu71@gmail.com>
2018-05-28 22:44:56 -0400

    update P03

--- P03.scala
+++ P03.scala
@@ -1,9 +1,10 @@
 
 object P03 extends App {
-  def kth(k: Int, l: List[Int]): Int = {
-    val size = l.size
-    if (k < size) l(k) else l(size-1)
+  def kth[T](k: Int, l: List[T]): T = k match {
+    case 0 => l.head
+    case _ => kth(k-1, l.tail)
   }
 
   println(kth(2, List(1,1,2,3,5,8)))
+  println(kth(2, List("a","b","e","c","d","z")))
 }
